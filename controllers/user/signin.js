b08da9ca553735abb121c83295177f4b53d5fe23
AzureSphere <dustin.jason.lee@gmail.com>
2016-05-04 16:30:14 +0200

    Handle wrong  passed parametres name for signin controller, send email to validete the email when trying to signin and the account in not validated

--- controllers/user/signin.js
+++ controllers/user/signin.js
@@ -2,12 +2,13 @@ var jwt = require('jsonwebtoken');
 var config = require(require('path').dirname(require.main.filename) + '/config'); // get db config file
 var mongoose = require('mongoose');
 var user = require(require('path').dirname(require.main.filename) + '/models/user'); // get the mongoose model
+var sendEmail = require(require('path').dirname(require.main.filename) + '/controllers/user/sendEmail'); // get the mongoose model
 
 var SECRET = process.env.SECRET || config.SECRET;
 
 module.exports = function(req, res) {
-    myUsername = (req.body.username).toLowerCase();
-    if (myUsername) {
+    if ((req.body.username) && (req.body.password)) {
+        var myUsername = (req.body.username).toLowerCase();
         user.findOne({ username: myUsername }, function(err, user) {
             if (err) throw err;
             if (!user) {
@@ -26,21 +27,40 @@ module.exports = function(req, res) {
                         });
                     };
                     if (matched) {
-                        var token = jwt.sign(user, SECRET, {
-                            expiresIn: 60
-                        });
-                        res.status(200).send({
-                            success: true,
-                            message: 'Take this token :) ',
-                            token: token
-                        });
+                        if (user.meta.validated) {
+                            var token = jwt.sign(user, SECRET, {
+                                expiresIn: 60
+                            });
+                            res.status(200).send({
+                                success: true,
+                                message: 'Take this token :) ',
+                                token: token
+                            });
+                        } else {
+                            var idPayload = Math.floor((Math.random() * 1000000000000000) + 1);
+                            var payload = ({
+                                subject: 'verifyEmail',
+                                author: 'Najla KANOUT, Faisal KANOUT',
+                                id: idPayload,
+                                username: myUsername,
+                            });
+                            user.meta.id = idPayload;
+                            user.save(function(err) {
+                                if (err) throw err;
+                                sendEmail(payload, function(err) {
+                                    if (err) throw err;
+                                    res.status(200).send({
+                                        success: true,
+                                        message: 'Your account is not valid yet, We sent you an email to ' + myUsername + ' to validate you email address befor you can signin',
+                                    });
+                                });
+                            });
+                        };
                     };
                 });
             };
         });
-
     };
-
     if (!myUsername) {
         res.status(403).send({
             success: false,
