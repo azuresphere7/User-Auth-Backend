137090d85ba410090fecbd35af3ec272c8e83ba9
AzureSphere <dustin.jason.lee@gmail.com>
2016-05-04 11:44:39 +0200

    add payload subject when check token

--- controllers/user/verifyEmail.js
+++ controllers/user/verifyEmail.js
@@ -7,36 +7,43 @@ var SECRET_EMAIL = process.env.SECRET_EMAIL || config.SECRET_EMAIL;
 module.exports = function(req, res) {
     var token = req.param('id');
     if (token) {
-        jwt.verify(token, SECRET_EMAIL, function(err, decoded) {
+        jwt.verify(token, SECRET_EMAIL, function(err, payload) {
             if (err) {
                 res.status(403).send({
                     success: false,
                     message: 'Your token id is not valid !'
                 });
             } else {
-            	console.log(decoded);
-                user.findOne({
-                    $and: [{ username: decoded.username }, { 'meta.id': decoded.id}]
-                }, function(err, user) {
-                    if (err) throw err;
-                    if (!user) {
-                        res.status(500).send({
-                            success: false,
-                            message: 'There is no user with this information !'
-                        });
-                    };
-                    if (user) {
-                        user.meta.validated = true;
-                        user.save(function(err) {
-                            res.status(201).send({
-                                success: true,
-                                message: 'your account ' + user.username + ' is now activated'
+                if (payload.subject == 'verifyEmail') {
+                    user.findOne({
+                        $and: [{ username: payload.username }, { 'meta.id': payload.id }]
+                    }, function(err, user) {
+                        if (err) throw err;
+                        if (!user) {
+                            res.status(403).send({
+                                success: false,
+                                message: 'There is no user with this information !'
                             });
+                        };
+                        if (user) {
+                            user.meta.validated = true;
+                            user.save(function(err) {
+                                res.status(201).send({
+                                    success: true,
+                                    message: 'your account ' + user.username + ' is now activated'
+                                });
 
-                        });
-                    }
-                });
+                            });
+                        }
+                    });
 
+                } else {
+                    res.status(403).send({
+                        success: false,
+                        message: 'Unknown token subject !'
+                    });
+
+                }
             };
         });
 
